e881eb68835b20355d124e981525640255fdf144
Olaseni Bello <Olasenibello38@gmail.com>
2023-07-01 11:59:37 -0400

    Setting up servers

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const ejs = require("ejs");
 
 // Allows rendering of ejs
 app.set('view engine', 'ejs');
+
+app.get("/", (req,res)=>{
+     res.send("Welcome to my page");
+})
+
+app.listen(8080, function() {
+    console.log("Server started on port 3000");
+  });
